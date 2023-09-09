c0b9d8595a587e1f4bf394b1b690d850ab882521
peerapat_vgen <peerapat.vgen@gmail.com>
2023-09-09 17:46:01 +0700

    upload file 9/9/2566

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from './components/organs/Navbar'
+import SingIn from './components/pages/SingIn'
+
+const App = () => {
+  return (
+    <>
+      <Navbar/>
+      <SingIn/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/pages/SingIn.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const SingIn = ({}: Props) => {
+  return (
+    <section>SingIn</section>
+  )
+}
+
+export default SingIn
